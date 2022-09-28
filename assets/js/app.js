dd73e262143d23ed1d29648b61d23d90e84ff08a
Max <max_pirate@mail.ru>
2022-09-28 17:36:49 +0300

    init

---
+++ assets/js/app.js
@@ -0,0 +1,103 @@
+const header = document.querySelector("#header");
+const intro = document.querySelector("#intro");
+let about = document.querySelector("#about");
+let services = document.querySelector("#services");
+let works = document.querySelector("#works");
+let blog = document.querySelector("#blog");
+let footer = document.querySelector("#footer");
+let scrollOffset = window.scrollY;
+const navBar = document.querySelectorAll('#nav a')
+let weDo1 = document.querySelector("#wedo_1");
+let weDo2 = document.querySelector("#wedo_2");
+let weDo3 = document.querySelector("#wedo_3");
+
+/* Fixed Header and Menu Lightning */
+window.addEventListener("scroll", function () {
+  scrollOffset = this.scrollY;
+  checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+  let aboutH = about.getBoundingClientRect().top + scrollOffset
+  let servicesH = services.getBoundingClientRect().top + scrollOffset
+  let worksH = works.getBoundingClientRect().top + scrollOffset
+  let blogH = blog.getBoundingClientRect().top + scrollOffset
+  let footerH = footer.getBoundingClientRect().top + scrollOffset
+  let aboutBlock = navBar[0]
+  let servicesBlock = navBar[1]
+  let worksBlock = navBar[2]
+  let blogBlock = navBar[3]
+  let footerBlock = navBar[4]
+
+  function addClassToBlock(blockHeight, blockNav, block, addClass = 'active', startOffset = 20, endOffset = 100) {
+    if (scrollOffset >= blockHeight - startOffset && scrollOffset <= blockHeight + block.clientHeight - endOffset) {
+      blockNav.classList.add(addClass);
+      header.classList.add("fixed");
+    } else {
+      blockNav.classList.remove(addClass);
+    }
+  }
+
+  /* Disable Lightning and Fixed Menu*/
+  if (scrollOffset < aboutH) {
+    removeActiveClass()
+    header.classList.remove("fixed");
+  }
+
+  /* Lightning About */
+  addClassToBlock(aboutH, aboutBlock, about)
+
+  /* Lightning Services */
+  addClassToBlock(servicesH, servicesBlock, services)
+
+  /* Lightning Works */
+  addClassToBlock(worksH, worksBlock, works)
+
+  /* Lightning Works */
+  addClassToBlock(blogH, blogBlock, blog, 'active', 20, 200)
+
+  /* Lightning Contacts */
+  addClassToBlock(footerH, footerBlock, footer, 'active', 400, 0)
+}
+
+
+
+/* Smooth scroll */
+navBar.forEach(item => item.addEventListener('click', function (e) {
+  e.preventDefault()
+  removeActiveClass()
+  currentMenu = item
+  let block = document.querySelector(item.dataset.scroll)
+
+  block.scrollIntoView({
+    behavior: 'smooth'
+  });
+
+}))
+
+function removeActiveClass() {
+  navBar.forEach(item => item.classList.remove('active'))
+}
+
+/* Collapse */
+function collapseWeDo(item) {
+  item.addEventListener('click', () => {    
+    if ([...item.classList].some(i => i !== 'active')) {
+      [weDo1, weDo2, weDo3].forEach(item => item.classList.remove('active'));
+      item.classList.add('active');
+    }
+  })
+}
+
+collapseWeDo(weDo1)
+collapseWeDo(weDo2)
+collapseWeDo(weDo3)
+
+/* Slider */
+$("[data-slider]").slick({
+  infinite: true,
+  fade: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
